1f364183f9947d3f473f7259afd4cb568e66356e
Himanshu Patel <himanshupatel13395@gmail.com>
2023-01-10 18:25:44 +0530

    Code Update

--- assets/script.js
+++ assets/script.js
@@ -11,53 +11,84 @@ window.addEventListener('scroll', function () {
         }
     }
 });
+/* Quickview Modal */
+customElements.define('modal-opener', class extends HTMLElement {
+    constructor() {            
+        super();               
+        let modalClosebuttons = this.querySelectorAll('[data-modal-dismiss="modal"]');   
+        let modalbuttons = document.querySelectorAll('[data-modal-target]');   
+        this.body = document.querySelector('body');    
+        modalClosebuttons.forEach((element) => {
+            element.addEventListener('click', this.close.bind(this));            
+        });
+        modalbuttons.forEach((element) => {          
+            element.addEventListener('click', this.open.bind(this));                       
+        });                     
+    }     
+    open(event){     
+        event.preventDefault();         
+        var target = event.explicitOriginalTarget.dataset.modalTarget;
+        var modal = document.querySelector(target);       
+        this.body.classList.add('noscroll');
+        modal.classList.remove('hidden');
+        setTimeout(function () {
+            modal.classList.add('open');
+        }, 100)
+        clearTimeout();        
+    }
+    close(){              
+        this.body.classList.remove('noscroll');        
+        this.classList.remove('open');           
+        this.classList.add('hidden');            
+    }
+});
+/* End Quickview Modal */
 
 document.addEventListener('DOMContentLoaded', function () {
     //Modal js
-    var modalbuttons = document.querySelectorAll('[data-modal-target]');
-    var modalClosebuttons = document.querySelectorAll('[data-modal-dismiss]');
-    modalbuttons.forEach(function (modalbutton) {
-        modalbutton.addEventListener("click", function (event) {
-            event.stopPropagation();
-            var modalId = modalbutton.getAttribute('data-modal-target');
-            var modal = document.querySelector(modalId);
-            document.querySelector('body').classList.add('noscroll');
-            modal.classList.remove('hidden');
-            setTimeout(function () {
-                modal.classList.add('open');
-            }, 100)
-            clearTimeout();
-        });
-    });
-    //Close modal when it is clicked on modal close button
-    modalClosebuttons.forEach(function (modalClosebutton) {
-        modalClosebutton.addEventListener("click", function (event) {
-            event.stopPropagation();
-            var modal = modalClosebutton.closest('.modal.open');
-            document.querySelector('body').classList.remove('noscroll');
-            modal.classList.remove('open');
-            setTimeout(function () {
-                modal.classList.add('hidden');
-            }, 300)
-            clearTimeout();
-        });
-    });
-
-
+    // var modalbuttons = document.querySelectorAll('[data-modal-target]');
+    // var modalClosebuttons = document.querySelectorAll('[data-modal-dismiss]');
+    // modalbuttons.forEach(function (modalbutton) {
+    //     modalbutton.addEventListener("click", function (event) {
+    //         event.stopPropagation();
+    //         var modalId = modalbutton.getAttribute('data-modal-target');
+    //         var modal = document.querySelector(modalId);
+    //         document.querySelector('body').classList.add('noscroll');
+    //         modal.classList.remove('hidden');
+    //         setTimeout(function () {
+    //             modal.classList.add('open');
+    //         }, 100)
+    //         clearTimeout();
+    //     });
+    // });
+    // //Close modal when it is clicked on modal close button
+    // modalClosebuttons.forEach(function (modalClosebutton) {
+    //     modalClosebutton.addEventListener("click", function (event) {
+    //         event.stopPropagation();
+    //         var modal = modalClosebutton.closest('.modal.open');
+    //         document.querySelector('body').classList.remove('noscroll');
+    //         modal.classList.remove('open');
+    //         setTimeout(function () {
+    //             modal.classList.add('hidden');
+    //         }, 300)
+    //         clearTimeout();
+    //     });
+    // });
+ 
     // Close modal when it is clicked outside
-    document.addEventListener("click", function (event) {
-        var modals = document.querySelectorAll('.modal.open');
-        modals.forEach(function (modal) {
-            if (event.target !== modal && !modal.contains(event.target)) {
-                document.querySelector('body').classList.remove('noscroll');
-                modal.classList.remove('open');
-                setTimeout(function () {
-                    modal.classList.add('hidden');
-                }, 300)
-                clearTimeout();
-            }
-        });
-    });
+    // document.addEventListener("click", function (event) {
+    //     var modals = document.querySelectorAll('.modal.open');
+    //     modals.forEach(function (modal) {
+    //         if (event.target !== modal && !modal.contains(event.target)) {
+    //             document.querySelector('body').classList.remove('noscroll');
+    //             modal.classList.remove('open');
+    //             setTimeout(function () {
+    //                 modal.classList.add('hidden');
+    //             }, 300)
+    //             clearTimeout();
+    //         }
+    //     });
+    // });
     //Modal js end
 
 
